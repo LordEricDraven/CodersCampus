d77816df48472d4cee982088333620d1c9cd9d1a
LordEricDraven <c.flamion88@gmail.com>
2023-10-07 15:44:47 -0400

    U3L1 update

---
+++ CodersCampus/Unit3/src/com/coderscampus/objects/RunnableClass.java
@@ -0,0 +1,11 @@
+package com.coderscampus.objects;
+
+public class RunnableClass {
+
+	public static void main(String[] args) {
+		// Class -> Blueprint for an Object
+		// Object -> a Noun, it's an instantiation of a class
+
+	}
+
+}
