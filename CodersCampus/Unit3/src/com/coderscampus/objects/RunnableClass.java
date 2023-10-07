cdeace1ee73e9e39b7551b9ada07e472ea694e71
LordEricDraven <c.flamion88@gmail.com>
2023-10-07 15:53:17 -0400

    Completed Lesson

--- CodersCampus/Unit3/src/com/coderscampus/objects/RunnableClass.java
+++ CodersCampus/Unit3/src/com/coderscampus/objects/RunnableClass.java
@@ -5,7 +5,27 @@ public class RunnableClass {
 	public static void main(String[] args) {
 		// Class -> Blueprint for an Object
 		// Object -> a Noun, it's an instantiation of a class
-
+		
+		// Data type, variable name, assignment operation
+		int someNumber = 39;
+		
+		Human trevorPage = new Human();
+		
+		trevorPage.eyeColor = "brown";
+		trevorPage.gender = "male";
+		trevorPage.height = "5'10\"";
+		trevorPage.weight = "160";
+		
+		System.out.println(trevorPage);
+		
+		Human elonMusk = new Human();
+		
+		elonMusk.eyeColor = "blue";
+		elonMusk.gender = "male";
+		elonMusk.height = "6'1\"";
+		elonMusk.weight = "190";
+		
+		System.out.println(elonMusk);
 	}
 
 }
