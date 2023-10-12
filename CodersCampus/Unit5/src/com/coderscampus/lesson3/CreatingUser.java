7f009d03583c846dbc380fa2e7c3aeb90d5f5678
LordEricDraven <c.flamion88@gmail.com>
2023-10-12 16:50:20 -0400

    completing Unit 5 first exercise
    Create a User Pojo

--- CodersCampus/Unit5/src/com/coderscampus/lesson3/CreatingUser.java
+++ CodersCampus/Unit5/src/com/coderscampus/lesson3/CreatingUser.java
@@ -5,7 +5,16 @@ public class CreatingUser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		User user = new User();
+		
+		PojoUser newUser = new PojoUser();
+		newUser.setUsername("LordEricDraven");
+		System.out.println("The username is: " + newUser.getUsername());
+		newUser.setPassword("P@$$w0rd!");
+		System.out.println("The password is: " + newUser.getPassword());
+		newUser.setName("Chris");
+		System.out.println("The name is: " + newUser.getName());
+		
+		
 		
 	}
 
