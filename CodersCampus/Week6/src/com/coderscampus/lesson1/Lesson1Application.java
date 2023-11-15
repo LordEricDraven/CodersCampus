6e7247f3778b4c1270a32eebf84b23191c80b122
LordEricDraven <c.flamion88@gmail.com>
2023-11-15 18:28:47 -0500

    finished U6L2

--- CodersCampus/Week6/src/com/coderscampus/lesson1/Lesson1Application.java
+++ CodersCampus/Week6/src/com/coderscampus/lesson1/Lesson1Application.java
@@ -5,13 +5,17 @@ public class Lesson1Application {
 	public static void main(String[] args) {
 
 		PhysicsTeacher physicsTeacher = new PhysicsTeacher();
-		
 		System.out.println(physicsTeacher.getClasses());
 		System.out.println(physicsTeacher.getLevelOfEducation());
 		
 		CompSciTeacher compSciTeacher = new CompSciTeacher();
 		System.out.println(compSciTeacher.getClasses());
 		System.out.println(compSciTeacher.getLevelOfEducation());
+		
+		QuantumPhysicsTeacher quantumTeacher = new QuantumPhysicsTeacher();
+		System.out.println(quantumTeacher.getClasses());
+		System.out.println(quantumTeacher.getLevelOfEducation());
+		
 		
 		
 	}
