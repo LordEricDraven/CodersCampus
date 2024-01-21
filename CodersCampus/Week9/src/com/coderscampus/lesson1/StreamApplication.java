653f27fb534af6f35c5a306c52f65c00947fc56b
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-01-21 14:04:38 -0500

    completed Unit 9 Lesson 12

--- CodersCampus/Week9/src/com/coderscampus/lesson1/StreamApplication.java
+++ CodersCampus/Week9/src/com/coderscampus/lesson1/StreamApplication.java
@@ -1,7 +1,9 @@
 package com.coderscampus.lesson1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.IntSummaryStatistics;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,6 +12,27 @@ import java.util.stream.Collectors;
 public class StreamApplication {
 
 	public static void main(String[] args) {
+		List<List<Integer>> listOfNumbersList = new ArrayList<>();
+		listOfNumbersList.add(Arrays.asList(1, 2, 3)); 
+		listOfNumbersList.add(Arrays.asList(2, 3, 4));	
+		listOfNumbersList.add(Arrays.asList(3, 4, 5));
+		listOfNumbersList.add(Arrays.asList(4, 5, 6));
+		
+		IntSummaryStatistics sumStats = listOfNumbersList.stream()
+						 					 .flatMap(x -> x.stream())
+						 					 .mapToInt(Integer::intValue)
+//										The x -> x.intValue() is the same as Integer::intValue
+//						 					 .mapToInt(x -> x.intValue())
+						 					 .summaryStatistics();
+		System.out.println("Average: " + sumStats.getAverage());
+		System.out.println("Max: " + sumStats.getMax());
+		System.out.println("Min: " + sumStats.getMin());
+		System.out.println("Count: " + sumStats.getCount());
+//		Example2();
+//		Example1();
+	}
+
+	private static void Example2() {
 		List<Car> cars = new ArrayList<>();
 		
 		
@@ -60,7 +83,6 @@ public class StreamApplication {
 		
 		
 		
-//		example1();
 	}
 
 	private static void example1() {
