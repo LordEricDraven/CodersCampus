8a450bea838a033ca41dfbe58ff7cbe7f11e0e33
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-02-11 16:56:30 -0500

    completed Unit 10 Lesson 7
    Modifying Java Util Dates

--- CodersCampus/Week10/src/com/coderscampus/lesson6/DatesApplication.java
+++ CodersCampus/Week10/src/com/coderscampus/lesson6/DatesApplication.java
@@ -1,13 +1,41 @@
 package com.coderscampus.lesson6;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DatesApplication {
 
 	public static void main(String[] args) {
-
-		Date now = new Date();
+//		Lesson6();
 		
+		// This is deprecated, don't use this approach, use Calendar instead
+		Date jan012020 = new Date(124, 0, 1, 0, 0, 0);
+		Calendar cal = Calendar.getInstance();
+		System.out.println(jan012020);
+		
+		
+		Date someOtherDate = cal.getTime();
+		System.out.println(someOtherDate);
+		cal.set(2020, 0, 1, 15, 1, 5);
+		someOtherDate = cal.getTime();
+		System.out.println(someOtherDate);
+		
+		cal.add(Calendar.HOUR, 2);
+		System.out.println(cal.getTime());
+		
+		cal.add(Calendar.MINUTE, -1);
+		System.out.println(cal.getTime());
+		
+		Date now = new Date();
+		if(now.after(cal.getTime())) {
+			System.out.println(now + " is after " + cal.getTime());
+		}
+	
+	}
+
+	private static void Lesson6() {
+		Date now = new Date();
 		System.out.println(now);
 		
 		java.sql.Date jan011970 = new java.sql.Date(86400000);
