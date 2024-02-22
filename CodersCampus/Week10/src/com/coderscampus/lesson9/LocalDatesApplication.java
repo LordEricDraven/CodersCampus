1de13b996f7cf864199961acbe2d93c65e4cce0b
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-02-22 13:36:46 -0500

    completed Unit 10 Lesson 10
    Java 8 Modifying Dates

--- CodersCampus/Week10/src/com/coderscampus/lesson9/LocalDatesApplication.java
+++ CodersCampus/Week10/src/com/coderscampus/lesson9/LocalDatesApplication.java
@@ -5,10 +5,27 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class LocalDatesApplication {
 
 	public static void main(String[] args) {
+//		Lesson9();
+		
+		LocalDateTime jan1 = LocalDateTime.of(2020,  1, 1, 13, 30, 0);
+		System.out.println(jan1.format(DateTimeFormatter.ofPattern("yyy-MM-dd h:mm:ss a")));
+		
+		LocalDateTime jan2 = LocalDateTime.of(2020, 1, 2, 9, 0, 0);
+		System.out.println("Is jan1 before jan2? " + jan1.isBefore(jan2));
+		
+		LocalDateTime jan3 = jan1.plusDays(2);
+		System.out.println("Is jan3 before jan2? " + jan3.isBefore(jan2));
+		
+		jan3 = jan3.minus(1, ChronoUnit.DAYS);
+		System.out.println(jan3);
+	}
+
+	private static void Lesson9() {
 		LocalDate date = LocalDate.now();
 		LocalTime time = LocalTime.now();
 		LocalDateTime dateTime = LocalDateTime.now();
