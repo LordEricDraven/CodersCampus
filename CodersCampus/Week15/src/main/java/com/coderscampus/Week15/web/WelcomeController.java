91f0ef2b5ead97e314ff1618ac55bc78ef068968
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-03-21 18:55:14 -0400

    Completed unit 15 lesson 1 and 2
    1 - Creating a plain old controller
    2 - Introduction to Thymeleaf and the Model

---
+++ CodersCampus/Week15/src/main/java/com/coderscampus/Week15/web/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.coderscampus.Week15.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("")
+	public String getRootWebpage(ModelMap model) {
+		// This is what gets returned/resolved when we
+		//	return a String inside of a regular Controller GetMapping method
+		// /src/main/resources/templates/{fileName}.html
+		String firstName = "Chris";
+		String lastName = "Flamion";
+		model.put("firstName", firstName);
+		model.put("lastName", lastName);
+		
+		return "welcome";
+	}
+	
+}
