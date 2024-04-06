4fe93f3df3caf082449189f4236fb0c72333be12
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-04-06 14:53:26 -0400

    completed unit 16 lesson 6

---
+++ CodersCampus/Unit 16/Lesson 6.sql
@@ -0,0 +1,18 @@
+-- Create, Read, Update, Delete
+
+insert into users (user_id, username, password, first_name, last_name)
+values (1, 'trevorpage@craftycodr.com', 'asdfasdf', 'Trevor', 'Page');
+
+insert into users (user_id, username, password, first_name, last_name)
+values (2, 'john@doe.com', 'password', 'John', 'Doe');
+
+insert into users (user_id, username, password, first_name, last_name)
+values (3, 'jane@doe.com', 'someOtherPassword', 'Jane', 'Doe');
+
+insert into users (user_id, username, password, first_name, last_name)
+values (4, 'elon@musk.com', 'asdfasdf', 'Elon', 'Musk');
+
+insert into users (user_id, username, password, first_name, last_name)
+values (5, 'jane@musk.com', 'asdfasdf', 'Jane', 'Musk');
+
+
