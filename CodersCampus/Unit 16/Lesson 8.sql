bb52617cf304f8cb576b9358da33faadb031dec8
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-04-06 20:37:52 -0400

    Completed unit 16 Lesson 8

---
+++ CodersCampus/Unit 16/Lesson 8.sql
@@ -0,0 +1,17 @@
+-- RDBMS -> Relational DataBase Management System
+--       -> MySQL, PostgreSQL, MS SQL Server, Oracle
+
+-- Relationships exist between tables (3 types):
+--   One-to-Many  (relationship)
+--   Many-to-Many (relationship)
+--   one-to-one   (relationship)
+
+-- User		Transaction
+--  1          1, 2
+--  2        3, 4, 5, 6
+--  3          7, 8
+
+select * from users;
+
+select * from transactions;
+
