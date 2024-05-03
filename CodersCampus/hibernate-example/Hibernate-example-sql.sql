d0e1fecfa2a6d6c739095a60ee9b960561b22df8
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-05-03 18:41:57 -0400

    updating lessons

---
+++ CodersCampus/hibernate-example/Hibernate-example-sql.sql
@@ -0,0 +1,9 @@
+create database hibernate_example;
+
+select * from users;
+
+insert into users (name, password, username)
+values ('Trevor Page', 'asdfasdf', 'trevor@craftycodr.com');
+
+insert into users (name, password, username)
+values ('Elon Musk', 'asdfasdf', 'elon@spacex.com');
