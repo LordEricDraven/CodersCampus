242d764cd19e53ed08ac90c467e38e7c55932126
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-07-10 18:58:17 -0400

    initial submit for Unit 21

---
+++ CodersCampus/week21/src/main/java/com/coderscampus/week21/web/LoginController.java
@@ -0,0 +1,19 @@
+package com.coderscampus.week21.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String getLogin (ModelMap model) {
+		User user = new User();
+		
+		model.put("user", user);
+		
+		return "login";
+	}
+	
+}
