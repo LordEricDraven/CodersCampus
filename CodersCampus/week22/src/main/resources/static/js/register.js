4196281a10a1d79cb88377e60a0c0b7ac1dbc0b1
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-07-28 18:57:20 -0400

    unit 22 Lesson 5 update
    05. Callbacks

--- CodersCampus/week22/src/main/resources/static/js/register.js
+++ CodersCampus/week22/src/main/resources/static/js/register.js
@@ -18,6 +18,26 @@ usernameTextbox.addEventListener('blur', () => {
 			console.log("Oops, this username already exists")
 			usernameTextbox.focus()
 			usernameTextbox.select()
-		}
+			showErrorAnimation(() => {
+				//animation is completed at this point
+				console.log("we're now in the callback function")
+				usernameTextbox.style.backgroundColor = 'rgb(255,255,255)'
+			})
+		} 
 	})
 })
+
+function showErrorAnimation(callback){
+	console.log("we're in the showErrorAnimation function")
+	var i = 0
+	
+	var animationInterval = setInterval(() => {
+		i++
+		usernameTextbox.style.backgroundColor = `rgb(255, ${255-i}, ${255-i})`
+		if (i === 255){
+			clearInterval(animationInterval);
+			console.log("Done Executing animation code")
+			callback()
+		}	
+	}, 1)
+}
