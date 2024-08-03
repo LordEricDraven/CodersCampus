3c3efbaae66bdbfdc5d81cbeee456aea93b10072
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-08-03 11:48:26 -0400

    unit 22 Lesson 8
    08. Async and Await

--- CodersCampus/week22/src/main/resources/static/js/register.js
+++ CodersCampus/week22/src/main/resources/static/js/register.js
@@ -20,6 +20,7 @@ eyeIcons.forEach((eyeIcon) => {
 			}
 		}
 	})
+	console.log
 })
 
 // This is for demonstration purposes
@@ -46,29 +47,33 @@ usernameTextbox.addEventListener('blur', () => {
 	var user = {
 		'username': usernameTextbox.value,
 	}
-	fetch('/users/exists', {
+	checkIfUserExists(user)
+	console.log("here we are after the fetch has completed")
+})
+
+async function checkIfUserExists(user){
+	let responseEntity = await fetch('/users/exists', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(user)
 	})
-	.then((responseEntity) => responseEntity.json())
-	.then( (data) => {
-		if (data === true){
-			// this users already exists!
-			console.log("Oops, this username already exists")
-			usernameTextbox.focus()
-			usernameTextbox.select()
-			showErrorAnimation().then((message) => {
-				//animation is completed at this point
-				console.log("we're now in the callback function")
-				console.log(message)
-				usernameTextbox.style.backgroundColor = 'rgb(255,255,255)'
-			})
-		} 
-	})
-})
+	let userExists = await responseEntity.json()
+	
+	if (userExists === true){
+		// this users already exists!
+		console.log("Oops, this username already exists")
+		usernameTextbox.focus()
+		usernameTextbox.select()
+		showErrorAnimation().then((message) => {
+			//animation is completed at this point
+			console.log("we're now in the callback function")
+			console.log(message)
+			usernameTextbox.style.backgroundColor = 'rgb(255,255,255)'
+		})
+	} 
+}
 
 function showErrorAnimation(){
 	return new Promise((resolve, reject) => {
